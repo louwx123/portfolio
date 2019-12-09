68314fc1ecb18b3fbdb1253898820b780b5ae62b
Weixiang Lou <weixianglou@WeixiangdeMacBook-Pro.local>
2019-12-08 19:45:21 -0500

    add data

---
+++ js/index.js
@@ -0,0 +1,293 @@
+
+const cherry1={ 
+  name="Cherry MX 8.0 Black",
+  id=101,
+  image="img/cherry/cherry-black-1.jpeg",
+  color="black",
+  brand="cherry",
+  price=200,
+  rating=4.8,
+  discount=10,
+  category="gaming",
+  sale="on sale",
+  stock=50,
+  light=true,
+}
+
+const cherry2={ 
+  name="Cherry G80 Black",
+  id=102,
+  image="img/cherry/cherry-black-2.jpeg",
+  color="black",
+  brand="cherry",
+  price=100,
+  rating=4.5,
+  discount=10,
+  category="business",
+  sale="on sale",
+  stock=180,
+  light=false,
+}
+
+const cherry3={ 
+  name="Cherry MX 6.0 Black",
+  id=103,
+  image="img/cherry/cherry-black-3.jpeg",
+  color="black",
+  brand="cherry",
+  price=170,
+  rating=4.8,
+  discount=20,
+  category="gaming",
+  sale="on sale",
+  stock=26,
+  light=true,
+}
+
+const cherry4={ 
+  name="Cherry MX 8.0 Pink",
+  id=104,
+  image="img/cherry/cherry-ping-4.jpeg",
+  color="pink",
+  brand="cherry",
+  price=220,
+  rating=4.8,
+  discount=10,
+  category="gaming",
+  sale=null,
+  stock=6,
+  light=false,
+}
+
+const cherry5={ 
+  name="Cherry MX 8.0 White",
+  id=105,
+  image="img/cherry/cherry-white-5.jpeg",
+  color="white",
+  brand="cherry",
+  price=200,
+  rating=4.8,
+  discount=10,
+  category="gaming",
+  sale="on sale",
+  stock=30,
+  light=true,
+}
+
+const cherry6={ 
+  name="Cherry G90 White",
+  id=106,
+  image="img/cherry/cherry-white-6.jpeg",
+  color="white",
+  brand="cherry",
+  price=100,
+  rating=4.5,
+  discount=20,
+  category="gaming",
+  sale=null,
+  stock=10,
+  light=false,
+}
+
+const cherry=[
+  cherry1,
+  cherry2,
+  cherry3,
+  cherry4,
+  cherry5,
+  cherry6,
+]
+const corsair1={ 
+  name="Corsair K63",
+  id=108,
+  image="img/corsair/corsair-black-1.jpeg",
+  color="black",
+  brand="corsair",
+  price=120,
+  rating=3.8,
+  discount=10,
+  category="gaming",
+  sale=null,
+  stock=20,
+  light=true,
+}
+
+const corsair2={ 
+  name="Corsair K65",
+  id=109,
+  image="img/corsair/corsair-black-1.jpeg",
+  color="black",
+  brand="corsair",
+  price=150,
+  rating=3.6,
+  discount=10,
+  category="gaming",
+  sale=null,
+  stock=10,
+  light=true,
+}
+
+const corsair3={ 
+  name="Corsair K69",
+  id=110,
+  image="img/corsair/corsair-black-1.jpeg",
+  color="black",
+  brand="corsair",
+  price=170,
+  rating=4.0,
+  discount=10,
+  category="gaming",
+  sale="on sale",
+  stock=0,
+  light=true,
+}
+
+const corsair4={ 
+  name="Corsair K70",
+  id=111,
+  image="img/corsair/corsair-black-1.jpeg",
+  color=["black", "white"],
+  brand="corsair",
+  price=220,
+  rating=4.8,
+  discount=null,
+  category="gaming",
+  sale="null",
+  stock=20,
+  light=true,
+}
+
+const corsair=[
+  corsair1,
+  corsair2,
+  corsair3,
+  corsair4,
+]
+
+const logitech1={ 
+  name="logitech G610",
+  id=112,
+  image="img/logitech/logitech-black-1.jpeg",
+  color="black",,
+  brand="logitech",
+  price=160,
+  rating=4.7,
+  discount=40,
+  category="gaming",
+  sale="on sale",
+  stock=0,
+  light=true,
+}
+
+const logitech2={ 
+  name="logitech M32",
+  id=113,
+  image="img/logitech/logitech-black-2.jpeg",
+  color="black",
+  brand="logitech",
+  price=80,
+  rating=3.2,
+  discount=10,
+  category="business",
+  sale="on sale",
+  stock=200,
+  light=false,
+}
+
+const logitech3={ 
+  name="logitech G913",
+  id=114,
+  image="img/logitech/logitech-black-3.jpeg",
+  color="black",
+  brand="logitech",
+  price=240,
+  rating=4.7,
+  discount=10,
+  category="gaming",
+  sale="on sale",
+  stock=3,
+  light=true,
+}
+
+const logitech4={ 
+  name="logitech K510",
+  id=115,
+  image="img/logitech/logitech-white-4.jpeg",
+  color="white",
+  brand="logitech",
+  price=80,
+  rating=4.7,
+  discount=20,
+  category="business",
+  sale="on sale",
+  stock=0,
+  light=false,
+}
+
+const logitech=[
+  logitech1,
+  logitech2,
+  logitech3,
+  logitech4,
+]
+
+const razer1={ 
+  name="razer Huntsman Black",
+  id=116,
+  image="img/razer/razer-black-1.jpeg",
+  color="black",
+  brand="razer",
+  price=120,
+  rating=4.7,
+  discount=10,
+  category="gaming",
+  sale="on sale",
+  stock=20,
+  light=true,
+}
+
+const razer2={ 
+  name="razer Blackwodow Black",
+  id=117,
+  image="img/razer/razer-black-1.jpeg",
+  color="black",
+  brand="razer",
+  price=120,
+  rating=4.7,
+  discount=10,
+  category="gaming",
+  sale="on sale",
+  stock=20,
+  light=true,
+}
+
+const razer3={ 
+  name="razer Onata Pink",
+  id=117,
+  image="img/razer/razer-pink-3.jpeg",
+  color="pink",
+  brand="razer",
+  price=220,
+  rating=4.7,
+  discount=0,
+  category="gaming",
+  sale=null,
+  stock=2,
+  light=true,
+}
+
+const razer4={ 
+  name="razer Blackwidow white",
+  id=119,
+  image="img/razer/razer-white44.jpeg",
+  color="white",
+  brand="razer",
+  price=120,
+  rating=4.2,
+  discount=10,
+  category="gaming",
+  sale="on sale",
+  stock=50,
+  light=true,
+}
+
