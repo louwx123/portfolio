75a729e0ebc04a5bac2a9467ac21bb1f8815f5cf
Weixiang Lou <weixianglou@WeixiangdeMacBook-Pro.local>
2019-12-10 15:45:48 -0500

    fix bug

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
 const alldata=[
   {//1
     name:`Cherry MX 8.0 Black`,
-    productCode:101,
-    imageUrl:`img/cherry/cherry-black-1.jpg`,
+    code:101,
+    image:`img/cherry/cherry-black-1.jpg`,
     color:`black`,
     brand:`cherry`,
     price:200,
@@ -13,8 +13,8 @@ const alldata=[
     light:`light`
   },{//2
     name:`Cherry G80 Black`,
-    productCode:102,
-    imageUrl:`img/cherry/cherry-black-2.jpg`,
+    code:102,
+    image:`img/cherry/cherry-black-2.jpg`,
     color:`black`,
     brand:`cherry`,
     price:100,
@@ -25,8 +25,8 @@ const alldata=[
     light:`nolight`
   },{//3
     name:`Cherry MX 6.0 Black`,
-    productCode:103,
-    imageUrl:`img/cherry/cherry-black-3.jpg`,
+    code:103,
+    image:`img/cherry/cherry-black-3.jpg`,
     color:`black`,
     brand:`cherry`,
     price:170,
@@ -37,8 +37,8 @@ const alldata=[
     light:`light`
   },{//4
     name:`Cherry MX 8.0 Pink`,
-    productCode:104,
-    imageUrl:`img/cherry/cherry-pink-4.jpg`,
+    code:104,
+    image:`img/cherry/cherry-pink-4.jpg`,
     color:`pink`,
     brand:`cherry`,
     price:220,
@@ -49,8 +49,8 @@ const alldata=[
     light:`nolight`
   },{//5
     name:`Cherry MX 8.0 White`,
-    productCode:105,
-    imageUrl:`img/cherry/cherry-white-5.jpg`,
+    code:105,
+    image:`img/cherry/cherry-white-5.jpg`,
     color:`white`,
     brand:`cherry`,
     price:200,
@@ -62,8 +62,8 @@ const alldata=[
 
   },{//6
     name:`Cherry G90 White`,
-    productCode:106,
-    imageUrl:`img/cherry/cherry-white-6.jpg`,
+    code:106,
+    image:`img/cherry/cherry-white-6.jpg`,
     color:`white`,
     brand:`cherry`,
     price:100,
@@ -75,8 +75,8 @@ const alldata=[
 
   },{//7
     name:`Corsair K63`,
-    productCode:108,
-    imageUrl:`img/corsair/corsair-black-1.jpg`,
+    code:108,
+    image:`img/corsair/corsair-black-1.jpg`,
     color:`black`,
     brand:`corsair`,
     price:120,
@@ -87,8 +87,8 @@ const alldata=[
     light:`light`
   },{//8
     name:`Corsair K65`,
-    productCode:109,
-    imageUrl:`img/corsair/corsair-black-1.jpg`,
+    code:109,
+    image:`img/corsair/corsair-black-1.jpg`,
     color:`black`,
     brand:`corsair`,
     price:150,
@@ -99,8 +99,8 @@ const alldata=[
     light:`light`
   },{//9
     name:`Corsair K69`,
-    productCode:110,
-    imageUrl:`img/corsair/corsair-black-1.jpg`,
+    code:110,
+    image:`img/corsair/corsair-black-1.jpg`,
     color:`black`,
     brand:`corsair`,
     price:170,
@@ -111,8 +111,8 @@ const alldata=[
     light:`light`
   },{//10
     name:`Corsair K70`,
-    productCode:111,
-    imageUrl:`img/corsair/corsair-black-1.jpg`,
+    code:111,
+    image:`img/corsair/corsair-black-1.jpg`,
     color:[`black`, `white`],
     brand:`corsair`,
     price:220,
@@ -123,8 +123,8 @@ const alldata=[
     light:`light`
   },{//11
     name:`logitech G610`,
-    productCode:112,
-    imageUrl:`img/logitech/logitech-black-1.jpg`,
+    code:112,
+    image:`img/logitech/logitech-black-1.jpg`,
     color:`black`,
     brand:`logitech`,
     price:160,
@@ -135,8 +135,8 @@ const alldata=[
     light:`light`
   },{//12
     name:`logitech M32`,
-    productCode:113,
-    imageUrl:`img/logitech/logitech-black-2.jpg`,
+    code:113,
+    image:`img/logitech/logitech-black-2.jpg`,
     color:`black`,
     brand:`logitech`,
     price:80,
@@ -147,8 +147,8 @@ const alldata=[
     light:`nolight`,
   },{//13
     name:`logitech G913`,
-    productCode:114,
-    imageUrl:`img/logitech/logitech-black-3.jpg`,
+    code:114,
+    image:`img/logitech/logitech-black-3.jpg`,
     color:`black`,
     brand:`logitech`,
     price:240,
@@ -159,8 +159,8 @@ const alldata=[
     light:`light`,
   },{//14
     name:`logitech K510`,
-    productCode:115,
-    imageUrl:`img/logitech/logitech-white-4.jpg`,
+    code:115,
+    image:`img/logitech/logitech-white-4.jpg`,
     color:`white`,
     brand:`logitech`,
     price:80,
@@ -170,9 +170,9 @@ const alldata=[
     stock:0,
     light:`nolight`,
   },{  //15
-    name:`razer Huntsman Black`,
-    productCode:116,
-    imageUrl:`img/razer/razer-black-1.jpg`,
+    name:`razer Huntsman`,
+    code:116,
+    image:`img/razer/razer-black-1.jpg`,
     color:`black`,
     brand:`razer`,
     price:120,
@@ -182,9 +182,9 @@ const alldata=[
     stock:20,
     light:`light`
   },{//16
-    name:`razer Blackwodow Black`,
-    productCode:117,
-    imageUrl:`img/razer/razer-black-2.jpg`,
+    name:`razer Blackwodow`,
+    code:117,
+    image:`img/razer/razer-black-2.jpg`,
     color:`black`,
     brand:`razer`,
     price:120,
@@ -195,8 +195,8 @@ const alldata=[
     light:`light`,
   },{//17
     name:`razer Onata Pink`,
-    productCode:117,
-    imageUrl:`img/razer/razer-pink-3.jpg`,
+    code:117,
+    image:`img/razer/razer-pink-3.jpg`,
     color:`pink`,
     brand:`razer`,
     price:220,
@@ -206,9 +206,9 @@ const alldata=[
     stock:2,
     light:`light`,
   },{//18
-    name:`razer Blackwidow white`,
-    productCode:119,
-    imageUrl:`img/razer/razer-white-4.jpg`,
+    name:`razer Blackwidow`,
+    code:119,
+    image:`img/razer/razer-white-4.jpg`,
     color:`white`,
     brand:`razer`,
     price:120,
@@ -220,138 +220,85 @@ const alldata=[
   }
 ]
 
-
-
 const shoppingCart = [
-  {ID: 101, qty: 1}
+  {itemid: 101, qty: 1}
 ];
-
-const wishlist=[
-  {
-    ID:100, qty:1}
-  ];
-
 //functions
 
-
-
-
-function getShoesAsHtml(alldata) {
-  let heartButton=`<button type="button" class="heartBtn" data-code="${alldata.productCode}" id="heartBtn"><span class="material-icons">favorite</span></button>`;
-  let sale='';
-  let outOfStock=``;
+function getItemAsHtml(alldata) {
+  let sale=``;
   if(alldata.sale==`on sale` && alldata.stock > 0){
-    sale=`<small class="onsale_banner">On Sale!</small>`;
-    cartBtn=`<button type="button" class="productCardButton"  data-code="${alldata.productCode}" id="addtocart"> Add to Cart</button> `;
-  
+    sale=`<small class="onsale">On Sale!!!</small>`;
+    cartbtn=`<button type="button" class="cartbtn"  data-code="${alldata.code}" id="addtocart"> Add to Cart</button> `;
   }
   else{
-    onsale=`<small class="outofstock">Out of stock</small>`
+    sale=`<small class="soldout">Sold out</small>`
   }
 
   return `
-    <section class="productsCard"> 
-    <img src="${alldata.imageUrl}" alt="${alldata.name}">
-    ${sale}
-    <h1><strong>${alldata.name}</strong></h1>
-        <ul>
-          <li><h3>${alldata.category}</h3></li>
-          <li><h3>Price: $${alldata.price}</h3></li>
-          <li><p>Rating: ${alldata.rating}<p></li>
-        </ul>
-        ${cartBtn}
-        </section>`;
-}
+    <section class="item"> 
+      <img src="${alldata.image}" alt="${alldata.name}">
+      ${sale}
+      <ul>
+        <li><label class="itemname">${alldata.name}</label></li>
+        <li><label>Price: $${alldata.price}</label></li>
+        <li><label>${alldata.category} keyboard</label></li>
+        <li><label>Rating: ${alldata.rating}<label></li> 
+      </ul>
+      ${cartbtn}
+    </section>`;
+} 
 
-document.getElementById(`app`).innerHTML = alldata
-  .map(getShoesAsHtml)
+document.getElementById(`allitems`).innerHTML = alldata
+  .map(getItemAsHtml)
   .join("\n");
 
-  //search functionality
-function LoadProductsByName(e){
-  e.preventDefault();
-  const search=document.getElementById('find').value;
-  const lowerCase=search.trim().toLowerCase();
-  const searchResultsArray=alldata.filter(p=> p.name.toLowerCase().includes(lowerCase));
+  //search 
+function LoadProducts(n){
+  const search=document.getElementById('find').value.toLowerCase();
+  const searchResultsArray=alldata.filter(item=> item.name.toLowerCase().includes(search));
   renderProducts(searchResultsArray);
 }
 
 //addto cart function
-function handleClicksofBtns(){
-  if (event.target.matches('button.productCardButton')) {
-    const code = parseInt(event.target.dataset.code);
-    addItemToCart(code);
-  }
 
-  if (event.target.closest('button').matches('button.heartBtn')) {
-    const code = parseInt(event.target.closest('button').dataset.code);
-      addToWishlist(code);
-  }
-  else{
-    if(!event.target.matches('button.heartBtn') && !event.target.matches('button.productCardButton')){
-      return;
-    }
-  
-  
-}}
+// const addItemToCart = itemid => {
 
+//   const cartItem = shoppingCart.find(item => item.itemid == itemid);
 
-
-const addToWishlist = pID => {
- 
-
-  const cartItem = wishlist.find(item => item.ID == pID);
-
-  if (cartItem) {  // if a cartItem was found
-    cartItem.qty++;
-    alert("Item has been already added to wishlist ");
-  } else {
-    wishlist.push({ID: pID, qty: 1});
-    alert("New wishlist item has been added wishlist");
-  }
-}
-
-//add to wishlist
-
-const addItemToCart = pID => {
-  // [ {courseid: 101, qty: 1} ]
-
-  const cartItem = shoppingCart.find(item => item.ID == pID);
-
-  if (cartItem) {  // if a cartItem was found
-    cartItem.qty++;
-    alert("Another item has been added to your cart");
-  } else {
-    shoppingCart.push({ID: pID, qty: 1});
-    alert("A new item has been added to your cart");
-  }
-}
-
-
-
-function LoadFilterMenu(){
-  const x = document.getElementById("filtermenu");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
-}
-//filter functions
-
-// function LoadProductsByPriceFilter(){
-//  const price=document.getElementById('PriceFilter').value;
-//  const searchResultsArray=alldata.filter(p=> p.price <= price);
-//  renderProducts(searchResultsArray);
+//   if (cartItem) {  
+//     cartItem.qty++;
+//     alert("Another item has been added to your cart");
+//   } else {
+//     shoppingCart.push({ID: pID, qty: 1});
+//     alert("A new item has been added to your cart");
+//   }
 // }
 
+// const addItemToCart = courseid => {
+//   // [ {courseid: 101, qty: 1} ]
+
+//   const cartItem = shoppingCart.find(item => item.courseid == courseid);
+
+//   if (cartItem) {  // if a cartItem was found
+//     cartItem.qty++;
+//   } else {
+//     shoppingCart.push({courseid: courseid, qty: 1});
+//   }
 
 
+function hideFilter(){
+  const hide = document.getElementById("filters");
+  if (hide.style.display == "none") {
+    hide.style.display = "block";
+  } else {
+    hide.style.display = "none";
+  }
+}
+
+//filte
 function LoadProductsByFilters(){
-  //heirarchy category>rating >brand> colour > size >  discount 
-  // let category;
-  // let brand;
-  // let color;
+
   let arrayByCategory=[];
   let arrayByBrand=[];
   let arrayByColor=[];  
@@ -359,7 +306,7 @@ function LoadProductsByFilters(){
 
   if(document.querySelector('input[name="category"]:checked')){
     let category=document.querySelector('input[name="category"]:checked').value;
-    arrayByCategory=alldata.filter(p=>p.category.toLowerCase()==category.toLowerCase());
+    arrayByCategory=alldata.filter(item=>item.category.toLowerCase()==category.toLowerCase());
   }
   else {
     arrayByCategory=alldata;
@@ -367,23 +314,17 @@ function LoadProductsByFilters(){
 
   if(document.querySelector('input[name="brand"]:checked')){
     let brand=document.querySelector('input[name="brand"]:checked').value;
-    arrayByBrand=arrayByCategory.filter(p=>p.brand.toLowerCase() == brand.toLowerCase());
+    arrayByBrand=arrayByCategory.filter(item=>item.brand.toLowerCase() == brand.toLowerCase());
   }
   else{
     arrayByBrand=arrayByCategory;
   }
 
-  // if(document.querySelector('input[name="price"]:checked') != null){
-  //   let price=document.querySelector('input[name="price"]:checked').value;
-  //   arrayByPrice=arrayByCategory.filter(p=>p.price<=price);
-  // }
-  // else{
-  //   arrayByPrice=arrayByCategory;
-  // }
+
 
   if(document.querySelector('input[name="color"]:checked')){
     let color=document.querySelector('input[name="color"]:checked').value;
-    arrayByColor=arrayByBrand(p=>p.color.toLowerCase() == color.toLowerCase());
+    arrayByColor=arrayByBrand.filter(p=>p.color==color);
   }
   else{
     arrayByColor=arrayByBrand;
@@ -398,13 +339,20 @@ function LoadProductsByFilters(){
     arrayByLight=arrayByColor;
   }
 
+  if(document.querySelector('input[name="price"]:checked') != null){
+    let price=document.querySelector('input[name="price"]:checked').value;
+    arrayByPrice=arrayByLight.filter(item=>item.price<=price);
+  }
+  else{
+    arrayByPrice=arrayByLight;
+  }
+
   renderProducts(arrayByLight);
 }
 
 
-//sorting functions
+//sort
 function LoadProductsPriceHighTolow() {
-  //alert("in high to low");
   const PriceHighToLowArray = alldata
     .slice(0)
     .sort((a, b) => (a.price < b.price ? 1 : -1));
@@ -412,7 +360,6 @@ function LoadProductsPriceHighTolow() {
 }
 
 function LoadProductsPriceLowToHigh() {
-  //alert("in low to high");
   const PriceLowToHighArray = alldata
     .slice(0)
     .sort((a, b) => (a.price > b.price ? 1 : -1));
@@ -420,7 +367,6 @@ function LoadProductsPriceLowToHigh() {
 }
 
 function LoadProductsByAvailability() {
-  //alert("in availability function");
   const availabilityArray = alldata
     .slice(0)
     .sort((a, b) => (a.stock < b.stock ? 1 : -1));
@@ -429,16 +375,12 @@ function LoadProductsByAvailability() {
 
 
 function renderProducts(arr) {
-  document.getElementById(`app`).innerHTML = arr.map(getShoesAsHtml).join("\n");
-  let res = 'Products';
-  if (arr.length == 1) {
-    res = 'Product'
-    document.getElementById('numResults').innerHTML = `(${arr.length} ${res})`;
+  document.getElementById(`allitems`).innerHTML = arr.map(getItemAsHtml).join("\n");
+  if (arr.length != 0) {
+    document.getElementById(`result`).innerHTML=`<label>${arr.length} results have been found</label>`
   }
- if(arr.length==0){
-  res = 'Products'
-    document.getElementById('numResults').innerHTML = `(${arr.length} ${res})`;
-    document.getElementById(`app`).innerHTML=`<h1>O RESULTS found!</h1>`
+  else{
+    document.getElementById(`result`).innerHTML=`<label>O result found!</label>`
  }
  
 }
@@ -448,20 +390,17 @@ function renderProducts(arr) {
 
 //execution
 window.addEventListener('load', () => {
-document.getElementById(`find`).addEventListener("change",LoadProductsByName);
-document.getElementById(`PriceHighToLow`).addEventListener("click", LoadProductsPriceHighTolow);
-document.getElementById(`PriceLowToHigh`).addEventListener("click", LoadProductsPriceLowToHigh);
+document.getElementById(`find`).addEventListener("change",LoadProducts);
+document.getElementById(`pricehightolow`).addEventListener("click", LoadProductsPriceHighTolow);
+document.getElementById(`pricelowtohigh`).addEventListener("click", LoadProductsPriceLowToHigh);
 document.getElementById(`availability`).addEventListener("click", LoadProductsByAvailability);
+document.getElementById(`filter`).addEventListener("click", hideFilter);
+document.querySelectorAll('[name="category"]').forEach(filterradio => filterradio.addEventListener('change', LoadProductsByFilters));
+document.querySelectorAll('[name="brand"]').forEach(filterradio => filterradio.addEventListener('change', LoadProductsByFilters));
+document.querySelectorAll('[name="color"]').forEach(filterradio => filterradio.addEventListener('change', LoadProductsByFilters));
+document.querySelectorAll('[name="light"]').forEach(filterradio => filterradio.addEventListener('change', LoadProductsByFilters));
+document.querySelectorAll('[name="price"]').forEach(filterradio => filterradio.addEventListener('change', LoadProductsByFilters));
 
-document.getElementById(`filter`).addEventListener("click", LoadFilterMenu);
-document.getElementById('app').addEventListener("click", handleClicksofBtns);
-document.querySelectorAll('[name="category"]').forEach(chkbox => chkbox.addEventListener('change', LoadProductsByFilters));
-document.querySelectorAll('[name="brand"]').forEach(chkbox => chkbox.addEventListener('change', LoadProductsByFilters));
-document.querySelectorAll('[name="color"]').forEach(chkbox => chkbox.addEventListener('change', LoadProductsByFilters));
-document.querySelectorAll('[name="light"]').forEach(chkbox => chkbox.addEventListener('change', LoadProductsByFilters));
-document.querySelectorAll('[name="price"]').forEach(chkbox => chkbox.addEventListener('change', LoadProductsByFilters));
-
-//start
-getShoesAsHtml(alldata);
+getItemAsHtml(alldata);
 
 });
